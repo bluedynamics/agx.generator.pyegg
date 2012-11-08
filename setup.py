cd42fa0968a3b53f0d148aa5eb97a285fd236e3a
Christoph Scheid <c@shri.de>
2012-11-08 23:24:34 +0100

    flake8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 import sys, os
 
 version = '1.0'
-shortdesc ="AGX Generator for python eggs"
+shortdesc = "AGX Generator for python eggs"
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read()
 
 setup(name='agx.generator.pyegg',
@@ -16,7 +16,7 @@ setup(name='agx.generator.pyegg',
             'Development Status :: 3 - Alpha',
             'License :: OSI Approved :: GNU General Public License (GPL)',
             'Operating System :: OS Independent',
-            'Programming Language :: Python', 
+            'Programming Language :: Python',
       ],
       keywords='AGX, Code Generator, Python eggs',
       author='BlueDynamics Alliance',
@@ -24,7 +24,7 @@ setup(name='agx.generator.pyegg',
       url=u'https://svn.plone.org/svn/archetypes/AGX',
       license='GNU General Public Licence',
       packages=find_packages('src'),
-      package_dir = {'': 'src'},
+      package_dir={'': 'src'},
       namespace_packages=['agx', 'agx.generator'],
       include_package_data=True,
       zip_safe=False,
@@ -35,7 +35,7 @@ setup(name='agx.generator.pyegg',
           'node.ext.uml',
           'agx.transform.uml2fs',
       ],
-      extras_require = dict(
+      extras_require=dict(
           test=[
             'interlude',
             'agx.generator.uml',
